5b25ab5de2ed66ee6ee1b24e16a57c47ddb155a5
deepakbaghelweb <baghel.deepak42@gmail.com>
2020-07-15 17:59:47 -0500

    Bug fixes 2

---
+++ asset/js/app.js
@@ -0,0 +1,22 @@
+var app = angular.module('fmbApp', []);
+            app.controller('ProductCtrl', function($scope, $http) { 
+              $http.get("https://spreadsheets.google.com/feeds/list/1UgxUvqufnRyc4MiIQ3xuYhH0p17DO3DSII3jNp1D0OE/1/public/values?alt=json")
+                    .success(function(response) {
+                    $scope.tools = response.feed.entry;
+                    $scope.fishes=[];$scope.chickens=[];$scope.seafoods=[];
+                  for (i=0; i<$scope.tools.length; i++){
+                      if($scope.tools[i].gsx$available.$t == "y"){
+                          if($scope.tools[i].gsx$category.$t == "fish"){
+                             $scope.fishes.push($scope.tools[i]);
+                           }
+                          if($scope.tools[i].gsx$category.$t == "chicken"){
+                             $scope.chickens.push($scope.tools[i]);
+                           }
+                          if($scope.tools[i].gsx$category.$t == "seafood"){
+                             $scope.seafoods.push($scope.tools[i]);
+                           }
+                      }
+                  }
+              });
+                                
+            });
